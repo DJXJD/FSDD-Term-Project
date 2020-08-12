cab40cdc7124528307c26de920dc5b6eb67cb44c
BuildTools <unconfigured@null.spigotmc.org>
2020-08-12 07:19:02 -0400

    further developed gofishapp and added a little to gofishgame

--- src/goFishGame/GoFishApp.java
+++ src/goFishGame/GoFishApp.java
@@ -16,56 +16,94 @@ public class GoFishApp {
     private GoFishGame game;
 
     private GoFishApp(GoFishGame game) {
-        if (game != null) {
-            this.game = game;
-        } else {
-            throw new IllegalArgumentException("The app's game cannot be null");
-        }
+	if (game != null) {
+	    this.game = game;
+	} else {
+	    throw new IllegalArgumentException("The app's game cannot be null");
+	}
     }
 
     public void announceTurn() {
-        System.out.println("It is now " + game.getActivePlayer().getName() + "'s turn\n"
-                + "Press Enter when ready to pass them the machine");
-            waitForEnter();
-        clearConsole();
-        System.out.println("Hello " + game.getActivePlayer().getName() + "!\n"
-                + "Press Enter when ready to begin your turn");
-        waitForEnter();
+	System.out.println("It is now " + game.getActivePlayer().getName() + "'s turn\n"
+		+ "Press Enter when ready to pass them the machine");
+	waitForEnter();
+	clearConsole();
+	System.out.println("Hello " + game.getActivePlayer().getName() + "!\n"
+		+ "Press Enter when ready to begin your turn");
+	waitForEnter();
+    }
+
+    public void askForCardInteraction(Scanner scanner) {
+
+    }
+
+    public int getTargetIdFromUser(Scanner scanner) {
+	int target;
+	String query = "Enter the player you'd like to ask for a card form (";
+	int counter = 1;
+	for (Player p : game.getPlayers()) {
+	    if (p != game.getPlayers()[game.getPlayers().length - 1]) {
+		query += counter + ", ";
+		counter++;
+	    } else {
+		query += "or " + counter + "): ";
+	    }
+	}
+	clearScannerInputBuffer(scanner);
+	System.out.print(query);
+	try {
+	    target = scanner.nextInt();
+	} catch (InputMismatchException e) {
+	    System.out.println("You must reference the player by their number");
+	    clearScannerInputBuffer(scanner);
+	    target = getTargetIdFromUser(scanner);
+	}
+	if (!(target >= 1 && target <= counter)) {
+	    System.out.println("You must reference one of the specified players");
+	    clearScannerInputBuffer(scanner);
+	    target = getTargetIdFromUser(scanner);
+	}
+	return target;
+    }
+
+    public void clearScannerInputBuffer(Scanner scanner) {
+	scanner.reset();
     }
 
     public void waitForEnter() {
-        Scanner emptyScanner = new Scanner(System.in);
-        emptyScanner.nextLine();
+	Scanner emptyScanner = new Scanner(System.in);
+	emptyScanner.nextLine();
     }
 
     public void clearConsole() {
-        for (int i = 0; i < 50; i++) {
-            System.out.println("");
-        }
+	for (int i = 0; i < 50; i++) {
+	    System.out.println("");
+	}
     }
 
     public static GoFishGame createGameObject(Scanner scanner) {
-        System.out.print("Enter the number of players for the game: ");
-        GoFishGame game = null;
-        try {
-            game = new GoFishGame(scanner.nextInt());
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            game = createGameObject(scanner);
-        } catch (InputMismatchException e) {
-            System.out.println("The number of players must be a whole number");
-            scanner.nextLine();
-            game = createGameObject(scanner);
-        }
-        return game;
+	System.out.print("Enter the number of players for the game: ");
+	GoFishGame game = null;
+	try {
+	    game = new GoFishGame(scanner.nextInt());
+	} catch (IllegalArgumentException e) {
+	    System.out.println(e.getMessage());
+	    game = createGameObject(scanner);
+	} catch (InputMismatchException e) {
+	    System.out.println("The number of players must be a whole number");
+	    scanner.nextLine();
+	    game = createGameObject(scanner);
+	}
+	return game;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        GoFishApp gameApp = new GoFishApp(createGameObject(scanner));
-        gameApp.game.init();
-        System.out.println("\nWelcome, the " + gameApp.game.getNumPlayers() + " of you, to our Go Fish game!\n");
-        gameApp.announceTurn();
+	Scanner scanner = new Scanner(System.in);
+	GoFishApp gameApp = new GoFishApp(createGameObject(scanner));
+	gameApp.game.init();
+	System.out.println("\nWelcome, the " + gameApp.game.getNumPlayers() + " of you, to our Go Fish game!\n");
+	gameApp.announceTurn();
+	System.out.println(gameApp.getTargetIdFromUser(scanner));
 //	while (!gameApp.game.isFinished()) {
 //	    
 //	}
--- src/goFishGame/GoFishGame.java
+++ src/goFishGame/GoFishGame.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package goFishGame;
 
 /**
@@ -10,7 +5,7 @@ package goFishGame;
  * @author David Vejgman
  */
 public class GoFishGame {
-    
+
     private boolean finished = false;
     private int numPlayers;
     private int activePlayerIndex = 0;
@@ -40,7 +35,7 @@ public class GoFishGame {
     public Player[] getPlayers() {
 	return players;
     }
-    
+
     public Player getActivePlayer() {
 	return players[activePlayerIndex];
     }
@@ -48,11 +43,11 @@ public class GoFishGame {
     public Deck getDeck() {
 	return deck;
     }
-    
+
     public void init() {
 	players = new Player[numPlayers];
-	for (int i = 0; i < numPlayers; i ++) {
-	    players[i] = new Player("Player" + ( i + 1 ), new PlayerHand());
+	for (int i = 0; i < numPlayers; i++) {
+	    players[i] = new Player("Player" + (i + 1), new PlayerHand());
 	}
 	deck = new Deck();
 	deck.buildDeck();
@@ -63,7 +58,7 @@ public class GoFishGame {
 	    initialDeal(5);
 	}
     }
-    
+
     public boolean askForCard(int target, Rank rank) {
 	if (players[activePlayerIndex].hasCard(rank) == -1) {
 	    throw new IllegalArgumentException("Player must have at least one of the cards they are asking for");
@@ -77,17 +72,17 @@ public class GoFishGame {
 	goFish();
 	return false;
     }
-    
+
     public void goFish() {
 	moveCard(deck, 0, players[activePlayerIndex].getHand());
     }
-    
+
     public Rank has4Match() {
 	for (Card i : players[activePlayerIndex].getHandCardList()) {
 	    int matches = 0;
 	    for (Card j : players[activePlayerIndex].getHandCardList()) {
 		if (i.getRank().equals(j.getRank())) {
-		    matches ++;
+		    matches++;
 		}
 	    }
 	    if (matches >= 4) {
@@ -96,33 +91,33 @@ public class GoFishGame {
 	}
 	return null;
     }
-    
+
     public void tradeMatch4Score(Rank rank) {
-	for (int i = 0; i < 4; i ++) {
+	for (int i = 0; i < 4; i++) {
 	    players[activePlayerIndex].removeCard(players[activePlayerIndex].hasCard(rank));
 	}
 	players[activePlayerIndex].addToScore();
     }
-    
+
     public void endTurn() {
-	activePlayerIndex ++;
+	activePlayerIndex++;
 	if (activePlayerIndex >= numPlayers) {
 	    activePlayerIndex = 0;
 	}
     }
-    
+
     public void initialDeal(int numCardsPerPlayer) {
-	for (int i = 0; i < numCardsPerPlayer; i ++) {
-	    for (int j = 0; j < numPlayers; j ++) {
+	for (int i = 0; i < numCardsPerPlayer; i++) {
+	    for (int j = 0; j < numPlayers; j++) {
 		moveCard(deck, 0, players[j].getHand());
 	    }
 	}
     }
-    
+
     public void moveCard(GroupOfCards src, int index, GroupOfCards dest) {
 	Card card = src.getCard(index);
 	src.removeCard(index);
 	dest.AddCard(card);
     }
-    
+
 }
