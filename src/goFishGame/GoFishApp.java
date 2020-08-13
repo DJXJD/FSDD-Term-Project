22e61e37f5a654bcdda1101f0fb1dc1a283e8ef6
Daniel <crawdani@sheridancollege.ca>
2020-08-12 23:49:54 -0400

    commented part of the main class

--- src/goFishGame/GoFishApp.java
+++ src/goFishGame/GoFishApp.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  * the user's card. To be used as starting code in ICE 1
  *
  * @author Nick De Luca
- * @date june 14th 2020
+ * @date Aug 12th 2020
  */
 public class GoFishApp {
 
@@ -26,7 +26,8 @@ public class GoFishApp {
     }
 
     public void announceTurn() {
-        System.out.println("It is now " + game.getActivePlayer().getName() + "'s turn\n"
+        System.out.println("It is now " + game.getActivePlayer().getName() 
+                + "'s turn\n"
                 + "Press Enter when ready to pass them the machine");
         waitForEnter();
         clearConsole();
@@ -52,7 +53,8 @@ public class GoFishApp {
 
     public static String askPlayer() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the player name you'd like to ask for a card from: (such as 'Player1') ");
+        System.out.println("Enter the player name you'd like to ask for a "
+                + "card from: (such as 'Player1') ");
         String name = sc.nextLine();
         if (game.getTargetPlayer(name) == null) {
             System.out.println("The player does not exist, try again:");
@@ -70,12 +72,22 @@ public class GoFishApp {
         emptyScanner.nextLine();
     }
 
+    /**
+     * This method displays many new line characters to hide the last player's
+     * hand
+     */
     public void clearConsole() {
         for (int i = 0; i < 50; i++) {
             System.out.println("");
         }
     }
 
+    /**
+     * This method gets the rank that a player has entered to use to ask for
+     * from another player
+     * @param sc the scanner to receive input from the user
+     * @return a String value for the desired rank
+     */
     public static String getRank(Scanner sc) {
         System.out.println("Enter the rank of card to ask:  ");
         String rank = sc.nextLine();
@@ -92,6 +104,11 @@ public class GoFishApp {
         return rank;
     }
 
+    /**
+     * This method is used for a player to draw 5 cards from the deck if they
+     * run out of cards
+     * @param player the player to draw cards
+     */
     public static void drawFiveCards(Player player) {
         int remainder = 5;
         if (game.getDeck().getCardList().size() < remainder) {
@@ -106,6 +123,12 @@ public class GoFishApp {
         }
     }
 
+    /**
+     * This method is used to create an object of the GoFishGame class to hold
+     * the information of the game
+     * @param scanner a scanner to get user input
+     * @return the GoFishGame object for this game
+     */
     public static GoFishGame createGameObject(Scanner scanner) {
         System.out.print("Enter the number of players for the game: ");
         GoFishGame game = null;
@@ -122,6 +145,9 @@ public class GoFishApp {
         return game;
     }
 
+    /**
+     * This method determines the winner of the game from the player scores
+     */
     public static void getWinner() {
         Player winner = game.getPlayers().get(0);
         for (int i = 1; i < game.getPlayers().size(); i++) {
@@ -133,11 +159,17 @@ public class GoFishApp {
         System.out.println("The winner is " + winner.getName() + "!");
     }
 
+    /**
+     * This is the main method of the GoFishApp and is where most of the user
+     * input takes place as well as the main control of the game itself
+     * @param args the arguments passed by the command line
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         GoFishApp gameApp = new GoFishApp(createGameObject(scanner));
         game.init();
-        System.out.println("\nWelcome, the " + game.getNumPlayers() + " of you, to our Go Fish game!\n");
+        System.out.println("\nWelcome, the " + game.getNumPlayers() + " of you,"
+                + " to our Go Fish game!\n");
 
         while (!game.isFinished()) {
 
@@ -158,26 +190,32 @@ public class GoFishApp {
                     }
                 }
                 if (game.getActivePlayer().checkScore()) {
-                    System.out.println(game.getActivePlayer().getName() + " Scored!");
+                    System.out.println(game.getActivePlayer().getName() 
+                            + " Scored!");
                 }
                 System.out.println(game.getActivePlayer());
                 try {
                     String rank = getRank(scanner);
                     String target = askPlayer();
-                    int cardNum = game.askForCard(target, Rank.valueOf(rank.toUpperCase()));
+                    int cardNum = game.askForCard(target, 
+                            Rank.valueOf(rank.toUpperCase()));
                     if (cardNum == 0) {
-                        System.out.println("go Fish\n" + "You drew a " + game.goFish());
-                        if (game.getActivePlayer().getHandCardList().isEmpty()) {
+                        System.out.println("go Fish\n" + "You drew a " 
+                                + game.goFish());
+                        if (game.getActivePlayer().getHandCardList().isEmpty()){
                             drawFiveCards(game.getActivePlayer());
                         }
                     } else {
-                        System.out.println(target + " gave you " + cardNum + " " + rank);
-                        if (game.getTargetPlayer(target).getHandCardList().isEmpty()) {
+                        System.out.println(target + " gave you " + cardNum 
+                                + " " + rank);
+                        if (game.getTargetPlayer(target).getHandCardList()
+                                .isEmpty()) {
                             drawFiveCards(game.getTargetPlayer(target));
                         }
                     }
                     if (game.getActivePlayer().checkScore()) {
-                        System.out.println(game.getActivePlayer().getName() + " Scored!");
+                        System.out.println(game.getActivePlayer().getName()
+                                + " Scored!");
                     }
                     System.out.println(game.getActivePlayer());
 
